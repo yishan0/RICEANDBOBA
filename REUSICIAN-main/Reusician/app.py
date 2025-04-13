c1719aa129150539028deb77b95845932b0264ce
Yishan Lin <Sushibalinu@gmail.com>
2025-04-13 01:18:44 -0700

    Add files via upload

--- REUSICIAN-main/Reusician/app.py
+++ REUSICIAN-main/Reusician/app.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin, current_user
 from users import get_user, append_user
-
+import sqlite3
 app = Flask(__name__)
 app.secret_key = 'secret_key_for_demo'
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+def get_db():
+    conn = sqlite3.connect("posts.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
 
 class User(UserMixin):
     def __init__(self, id):
@@ -21,6 +27,10 @@ def load_user(user_id):
 
 @app.route('/')
 def home():
+    return render_template('about.html')
+
+@app.route('/about')
+def about():
     return render_template('about.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -77,13 +87,42 @@ def profile():
 def placeholder():
     return render_template('placeholder.html')
 
-@app.route('/support')
+@app.route('/support', methods=['GET', 'POST'])
 def support():
+    if request.method == 'POST':
+        print("Form data: ", request.form)
+        email = request.form.get('email')
+        name = request.form.get('name')
+        item = request.form.get('item')
+        value = request.form.get('value')
+        lf = request.form.get('lf')
+        info = request.form.get('info')
+
+
+        conn = get_db()
+        cursor = conn.cursor()
+        cursor.execute('''
+        INSERT INTO posts (email, name, my_item, looking_for, value, more_info)
+        VALUES (?, ?, ?, ?, ?, ?)
+        ''', (email, name, item, lf, value, info))
+
+        conn.commit()
+        conn.close()
+        # Add logic to handle the submitted form data
+        flash('Form submitted successfully!')
+        return redirect(url_for('support'))
     return render_template('support.html')
 
 @app.route('/forum')
 def forum():
-    return render_template('forum.html')
+    conn = get_db()
+    cursor = conn.cursor()
+
+    # Fetch all columns from the posts table
+    cursor.execute("SELECT * FROM posts")
+    rows = cursor.fetchall()
+    conn.close()
+    return render_template('forum.html', posts=rows)
 
 if __name__ == '__main__':
     app.run(debug=True)
